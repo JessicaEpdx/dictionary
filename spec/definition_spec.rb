a2c7c7d68e2dbd1808e48d66f778c5720a94bf9a
Jessicaepdx <Jessicaepdx@gmail.com>
2015-05-01 09:40:18 -0700

    clear, save, show all definitions

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,3 +1,32 @@
 require('rspec')
 require('word')
 require('definition')
+
+describe('Definition') do
+  before() do
+    Definition.clear()
+  end
+
+  describe('.clear') do
+    it("is empty at first") do
+      expect(Definition.clear()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a definition") do
+      definition = Definition.new("a saucy hamburger")
+      definition.save()
+      expect(Definition.all()).to(eq([definition]))
+    end
+  end
+
+  describe('.all') do
+    it("shows all definitions") do
+      definition = Definition.new("a cheesy pizza")
+      definition.save()
+      expect(Definition.all()).to(eq([definition]))
+    end
+  end
+
+end
